6c6a7efc0b88affe759844d7c55b1b31f620e5be
Daniel Manasseh <thatboy_west@ThatBoyWest.local>
2025-08-26 18:45:32 -0230

    updates

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
 import random, json, unicodedata, re, json
-from werkzeug import Response
 import os
 
 app = Flask(__name__,template_folder='templates', static_folder='static')
@@ -8,9 +7,11 @@ app.config['SECRET_KEY'] = 'acd3397cf45c2570af12e2412498db7ce3009341affd18d3'
 
 
 
-with open('football_riddles.json', 'r', encoding='utf-8') as f:
+# with open('football_riddles.json', 'r', encoding='utf-8') as f:
+#     riddles = json.load(f)
+with open(os.path.join(os.path.dirname(__file__), "football_riddles.json"), "r", encoding="utf-8") as f:
     riddles = json.load(f)
-
+    
 @app.route('/', methods = ['POST', 'GET'])
 def home():
     riddle = random.choice(riddles)
@@ -31,7 +32,7 @@ def send_riddles(difficulty):
 
 
 @app.route('/riddle/<get_id>/answer/', methods=['POST'])
-def check_answer(get_id) -> Response:
+def check_answer(get_id):
     riddle = next(filter(lambda r: r['id'] == get_id, riddles), None)
     answers = riddle['answers']
     guess_answer = request.form['answer']
@@ -42,7 +43,7 @@ def check_answer(get_id) -> Response:
     return redirect(url_for('home'))
 
 
-def normalize_name(s: str) -> str:
+def normalize_name(s: str):
     s = s.strip().lower()
     s = unicodedata.normalize('NFKD', s)
     s = ''.join(c for c in s if not unicodedata.combining(c))
@@ -50,10 +51,12 @@ def normalize_name(s: str) -> str:
     s = re.sub(r'[^a-z0-9]', '', s)  
     return s
 
-def matches(user_answer, correct_answers) -> bool:
+def matches(user_answer, correct_answers):
     normalized_user = normalize_name(user_answer)
     return any(map(lambda ans: normalized_user == normalize_name(ans), correct_answers))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
    
+app = app
